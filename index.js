f0ea9bc10efc494d9bdcfe6ebc5f6a83ecd715e6
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-01-04 10:10:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,31 @@ const scanner = require("tesseract.js");
 const client = discord.Client();
 
 const dotconfig = require("dotenv").config();
+const prefix = "=";
 
+client.on("ready", () => {
+    console.log("Bot aktiv");
+})
+
+client.on("message", async message => {
+    if (message.author.bot) return;
+    const guild = message.guild;
+    const pengirim = message.author;
+    const pesan = message.content.slice()
+
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content
+        .slice(prefix.length)
+        .trim()
+        .split(/ +/g);
+    const command = args.shift().toLowerCase();
+  
+    if (command === "teks") {
+         scanner.recognize(args[0], "eng", { logger: m => console.log(m) }).then(({ data: { text } }) => {
+             message.channel.send(text)
+         })
+    }
+})
+
+client.login("Nzc3NzU2NTAzMDI4NDAwMTM4.X7IEMA.Giia9XmQ7drYspNaqoYIz4nynew")
