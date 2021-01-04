4f29112218255fa6fb792e4cb5e3770a1ad1c4a2
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-01-04 11:55:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const discord = require("discord.js");
-const scanner = require("tesseract.js");
+const { createWorker } = require("tesseract.js");
 
 const client = new discord.Client();
 
 const dotconfig = require("dotenv").config();
-const prefix = "=";
+
+const prefix = "=";  //change it to whatever..
+
+const fileEXT = [
+    ".PNG",
+    ".png",
+    ".jpg",
+    ".jpeg"
+]
 
 client.on("ready", () => {
-    console.log("Bot aktiv");
+    console.log("Bot aktif");
 })
 
 client.on("message", async message => {
@@ -23,12 +31,39 @@ client.on("message", async message => {
         .trim()
         .split(/ +/g);
     const command = args.shift().toLowerCase();
-  
+
     if (command === "teks") {
-         scanner.recognize(args[0], "eng", { logger: m => console.log(m) }).then(({ data: { text } }) => {
-             return message.channel.send(text);
-         })
+        let link = args[0];
+        //if (link.endsWith(".png") || link.endsWith(".PNG")) {
+        if (message.attachments.size >= 1) {
+            console.log(message.attachments.size)
+            console.log(JSON.stringify(message.attachments))
+            link = message.attachments.array()[0].url
+        }
+        if (!link) return message.channel.send("Format: `" + prefix + "teks [link]`");
+
+        message.channel.send("Memproses... ").then(msg => {
+            const worker = createWorker({
+                logger: m => {
+                    if (!m.progress) return;
+                    if (m.progress == 0 || m.progress == 1 || m.progress == 0.5) return;
+                    
+                    return msg.edit(`Memproses, ${Math.floor(m.progress * 100)}%`);  //remove this line if you dont want to bypass Discord rate-limit                 
+                }
+            });
+
+            (async () => {
+                await worker.load();
+                await worker.loadLanguage('eng');
+                await worker.initialize('eng');
+                const { data: { text } } = await worker.recognize(link).catch(err => { msg.edit("Link harus berupa gambar!") });
+
+                msg.edit(`Hasil: ${"`" + text + "`"}`);
+                await worker.terminate();
+            })();
+        })
+        //}
     }
 })
 
-client.login("Nzc3NzU2NTAzMDI4NDAwMTM4.X7IEMA.Giia9XmQ7drYspNaqoYIz4nynew")
+client.login("Bot token here..")
